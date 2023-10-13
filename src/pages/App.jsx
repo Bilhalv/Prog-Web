44c995ac130dc31c7676d733bf5a27d1caaa66ac
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-10-13 18:49:34 -0300

    fix attempt

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
 import BasicModal from "../components/Form";
